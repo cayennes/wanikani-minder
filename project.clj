b0be7cc1ce78024e1d8046c95a2d00d0409712ab
Cayenne Geis <cayennes@gmail.com>
2017-08-04 19:23:16 -0400

    Update clj-http version
    Fixes SSL errors

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [clj-http "2.3.0"]
+                 [clj-http "3.6.1"]
                  [cheshire "5.6.3"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.9.7"]]
