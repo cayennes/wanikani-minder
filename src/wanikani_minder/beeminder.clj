c9b3bfbc915026922700f04ea85af64a643eb1bf
Cayenne Geis <cayennes@gmail.com>
2018-10-06 10:43:16 -0400

    Ability to correctly create goal from repl

--- src/wanikani_minder/beeminder.clj
+++ src/wanikani_minder/beeminder.clj
@@ -4,6 +4,11 @@
 
 (def api-url "https://www.beeminder.com/api/v1")
 
+;; TODO: consider some kind of automatic update for this
+(def total-wanikani-items (+ 447 ;; "radicals"
+                             2027 ;; kanji
+                             6300)) ;; vocabulary items
+
 (defn add-datapoint
   [{:keys [beeminder-access-token]} goal {:keys [value comment]}]
   (client/post (str api-url "/users/me/goals/" goal "/datapoints.json")
@@ -11,7 +16,37 @@
                               "comment" comment ;; TODO: make this optional
                               "access_token" beeminder-access-token}}))
 
+(defn create-goal
+  [{:keys [beeminder-access-token]} options]
+  (client/post (str api-url "/users/me/goals.json")
+               {:form-params (merge options
+                                    {"access_token" beeminder-access-token})}))
+
+(defn update-goal
+  [{:keys [beeminder-access-token]} goal data]
+  (client/put (str api-url "/users/me/goals/" goal ".json")
+              {:form-params (merge data
+                                   {"access_token" beeminder-access-token})
+               :redirect-strategy :lax}))
+
+(defn create-wanikani-minder-goal
+  [user
+   {:keys [slug rate start-value initial-safety-buffer]
+    :or {start-value 0
+         initial-safety-buffer 9}}]
+  (create-goal user {:slug slug
+                     :rate rate
+                     :goalval total-wanikani-items
+                     :initval start-value
+                     :datasource (get-in config [:beeminder :client-name])
+                     :goal_type "custom"})
+  (update-goal user slug {:odom true
+                          :aggday "max"
+                          :kyoom false
+                          :integery true}))
+
 ;; things below here are currently tools for the repl rather than used by the app
+;; get a user in repl with (wanikani-minder.db.user/get "beeminder-username")
 
 (defn user-info
   [{:keys [beeminder-access-token]}]
@@ -38,7 +73,6 @@
                                    {"access_token" beeminder-access-token})
                :redirect-strategy :lax}))
 
-;; get user with (wanikani-minder.db.user/get "beeminder-username")
 (defn register-autofetch
   [user goal]
   (let [source-name (get-in config [:beeminder :client-name])]
