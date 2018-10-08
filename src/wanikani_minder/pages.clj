35bbe1b6ad41da50bb29e5ca12bdd400503e2de6
Cayenne Geis <cayennes@gmail.com>
2018-10-08 16:28:12 -0400

    Add explanation

--- src/wanikani_minder/pages.clj
+++ src/wanikani_minder/pages.clj
@@ -3,12 +3,19 @@
             [hiccup.core :refer [html]]
             [ring.util.anti-forgery :as ring-af]))
 
+(def explanation
+  [:div
+   [:h2 "What this is"]
+   [:p "The is an integration to automatically track " [:a {:href "http://wanikani.com"} "WaniKani"] " progress with a " [:a {:href "http://beeminder.com"} "Beeminder"] " goal."]
+   [:p "The number tracked is the number of items that you have started studying and are caught up on reviews for; that is, they are not currently due. This is the number that represents real progress; if you only track the number you've started, then when you fall behind on reviews your number stays high but you might have actually forgotten everything and not made any true progress. This number is also useful when you're trying to work down a backlog of due items, since it's measuring the increase in caught up cards."]
+   [:p "It uses the highest number it gets for the day, so if you make sure you're on track, new items coming due will not cause you to derail later in the day."]])
+
 (defn logged-out-homepage
   [beeminder-authorize-url]
   (html [:div
          [:h1 "WaniKani Minder"]
-         [:p "Automatically beemind WaniKani progress"]
-         [:p [:a {:href beeminder-authorize-url} "Login via beeminder"]]]))
+         [:p [:a {:href beeminder-authorize-url} "Login via beeminder"]]
+         explanation]))
 
 (defn error-span
   [message]
@@ -55,7 +62,8 @@
              [:input {:type :text :id "rate" :name "rate"}]
              " "
              (if-let [e (:rate (:errors create-goal))] (error-span e))]
-            [:p [:button {:type :submit :name "action" :value "create-goal"} "Create"]]])]))
+            [:p [:button {:type :submit :name "action" :value "create-goal"} "Create"]]])
+         explanation]))
 
 (defn error
   [error error-description]
