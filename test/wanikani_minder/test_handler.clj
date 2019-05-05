b9ae40efcf2880ed3949468f0c3ea00d91fb974d
Cayenne Geis <cayennes@gmail.com>
2019-05-05 12:47:54 -0400

    Delete old url-minder tests

--- test/wanikani_minder/test_handler.clj
+++ test/wanikani_minder/test_handler.clj
@@ -4,15 +4,11 @@
             [ring.mock.request :as mock]
             [clj-http.client :as client]))
 
-(deftest routes-return-success
+(deftest basic-routes-return-success
   (with-redefs [handler/get-total (constantly 17)
                 handler/get-due-count (constantly 4)
                 client/get (fn [& _] (assert false "no internet allowed"))
-                client/post (fn [& _](assert false "no internet allowed"))]
+                client/post (fn [& _] (assert false "no internet allowed"))]
     (doseq [path-etc ["/"
-                      "/auth/beeminder/callback?access_token=a&username=b"
-                      "/wanikani-urlminder"
-                      "/wanikani-urlminder/user/somekey/backlog-reduction-from/20"
-                      "/wanikani-urlminder/user/somekey/total-studied"
-                      "/wanikani-urlminder/user/somekey/maintained-progress"]]
+                      "/auth/beeminder/callback?access_token=a&username=b"]]
       (is (= 200 (:status (handler/app (mock/request :get path-etc))))))))
